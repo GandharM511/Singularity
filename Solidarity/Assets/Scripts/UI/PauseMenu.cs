3095de8231e998ff3c1f96a4b3ef3fa88230be98
Richmond Ballesteros <richmond_tyler_ballesteros@yahoo.com>
2022-06-04 21:25:43 -0700

    Added pause menu but audio not persisting

---
+++ Solidarity/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+    public GameObject controlsMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+    
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void LoadOptions()
+    {
+        Debug.Log("Loading Options");
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void ReturnMainMenu()
+    {
+        Debug.Log("Loading Quit Game");
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu");
+    }
+}
